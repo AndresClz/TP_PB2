e4accd08ed59b4bf1b3440ec1b61c306b87b8f7b
Andres Calizaya <andres.u.calizaya@gmail.com>
2025-06-27 21:14:12 -0300

    test(profugo): agregados tests rojos para decoradores de evolución
    - Se definieron tests iniciales para verificar comportamiento de los decoradores
    - Aún no están implementadas las clases concretas, por lo que los tests fallarán

---
+++ test/ar/unlam/edu/ar/tp/ProfugoDecoradorTest.java
@@ -0,0 +1,30 @@
+package ar.unlam.edu.ar.tp;
+import static org.junit.Assert.*;
+
+import ar.unlam.edu.ar.tp.model.Profugo;
+import ar.unlam.edu.ar.tp.model.ProfugoBase;
+import org.junit.Test;
+
+public class ProfugoDecoradorTest {
+
+    @Test
+    public void queLaHabilidadSeDupliqueConArtesMarcialesSinSuperarCien() {
+        Profugo p = new ProfugoBase(30, 60, true);
+        Profugo entrenado = new ArtesMarcialesAvanzadas(p);
+        assertEquals(100, entrenado.getHabilidad());
+    }
+
+    @Test
+    public void queElProfugoConEntrenamientoEliteNuncaSeaNervioso() {
+        Profugo p = new ProfugoBase(30, 50, true);
+        Profugo entrenado = new EntrenamientoElite(p);
+        assertFalse(entrenado.isNervioso());
+    }
+
+    @Test
+    public void queElProfugoConProteccionLegalMantengaInocenciaMinimaDeCuarenta() {
+        Profugo p = new ProfugoBase(35, 50, true);
+        Profugo entrenado = new ProteccionLegal(p);
+        assertEquals(40, entrenado.getInocencia());
+    }
+}
